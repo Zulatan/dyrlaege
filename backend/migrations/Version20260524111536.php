30275d576bf5b5e3ef09783eb0e4ece4a84be30a
Mikkel Lau <mikkellau98@hotmail.com>
2026-06-01 11:03:33 +0200

    doctrine migrations

---
+++ backend/migrations/Version20260524111536.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260524111536 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP name, DROP surname');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `user` ADD name VARCHAR(15) DEFAULT NULL, ADD surname VARCHAR(30) DEFAULT NULL');
+    }
+}
