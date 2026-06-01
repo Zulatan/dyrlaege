38f003bc10a8568ab423948d977e674f23dfdef6
Mikkel Lau <mikkellau98@hotmail.com>
2026-06-01 13:22:11 +0200

    fixtures update

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 use App\Entity\User;
+use App\Enum\UserRole;
 use App\Enum\TaskPriority;
 use App\Enum\TaskStatus;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -22,8 +23,8 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
     
-        // create 20 products with random prices
-        for ($i = 0; $i < 5; $i++) {
+        // create 2 users with random values
+        for ($i = 0; $i < 2; $i++) {
 
             // Users for testing
             $user = new User();
@@ -42,6 +43,21 @@ class AppFixtures extends Fixture
             $manager->persist($user);
             $manager->persist($task);
         }
+        $password = $this->hasher->hashPassword($user, 'pass_1234');
+
+        // create users with roles
+        $vet = new User();
+        $vet->setEmail('vet@test.dk');
+        $vet->setRoles([UserRole::VET->value]);
+        $vet->setPassword($password);
+
+        $admin = new User();
+        $admin->setEmail('admin@test.dk');
+        $admin->setRoles([UserRole::ADMIN->value]);
+        $admin->setPassword($password);
+
+        $manager->persist($vet);
+        $manager->persist($admin);
 
         $manager->flush();
     }
